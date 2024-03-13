fd302145905e60f3b8ccd095af2a58d4e6b1fbff
Lukáš Polák <Polak.Lukas@stud.vassboskovice.cz>
2024-03-13 19:38:49 +0100

    Přidejte soubory projektu.

---
+++ Grafika_IT2/Board.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Grafika_IT2
+{
+  public class Board
+  {
+    private List<Point> points;
+
+    public Board()
+    {
+      points = new List<Point>();
+    }
+
+    public void Clear()
+    {
+      points.Clear();
+    }
+
+    public void Add(Point point)
+    {
+      points.Add(point);
+    }
+
+    public void Draw(Canvas canvas)
+    {
+      canvas.Children.Clear();
+      foreach (var point in points)
+      {
+        Ellipse ellipse = new Ellipse();
+        ellipse.Width = 10;
+        ellipse.Height = 10;
+        ellipse.Fill = Brushes.Blue;
+        Canvas.SetLeft(ellipse, point.X - (ellipse.Width / 2));
+        Canvas.SetTop(ellipse, point.Y - (ellipse.Height / 2));
+        canvas.Children.Add(ellipse);
+      }
+    }
+
+    public override string ToString()
+    {
+      string text = "";
+      foreach(var point in points)
+      {
+        text += $"{point.X};{point.Y}\r\n";
+      }
+      return text;
+    }
+
+    public void FromString(string text)
+    {
+      points.Clear();
+      foreach(var line in text.Split("\r\n"))
+      {
+        var parts = line.Split(";");
+        if(parts.Length == 2)
+        {
+          if (!double.TryParse(parts[0], out var x))
+          {
+            continue;
+          }
+          if(!double.TryParse(parts[1], out var y))
+          {
+            continue;
+          }
+          points.Add(new Point(x, y));
+        }        
+      }
+    }
+    
+  }
+}
---
+++ Grafika_IT2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grafika_IT2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private Board board;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      board = new Board();
+    }
+
+    private void canvasBoard_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      board.Add(e.GetPosition(canvasBoard));
+      board.Draw(canvasBoard);
+    }
+
+    private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+    {
+      var text = File.ReadAllText("data.csv");
+      board.FromString(text);
+      board.Draw(canvasBoard);
+    }
+
+    private void ButtonSave_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        File.WriteAllText("data.csv", board.ToString());
+        MessageBox.Show("Soubor byl uložen");
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show("Soubor nebyl uložen z důvodu chyby:\r\n" + ex.Message);
+      }     
+    }
+
+    private void ButtonClear_Click(object sender, RoutedEventArgs e)
+    {
+      board.Clear();
+      board.Draw(canvasBoard);
+    }
+  }
+}
